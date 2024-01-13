33bff9af887e87d68943f929751af33d7e21ff9f
bleclere <brice.leclere@univ-nantes.fr>
2024-01-13 15:52:54 +0100

    added lexer function

---
+++ R/lexer.R
@@ -0,0 +1,9 @@
+lexer <- function(makefile) {  
+    read <- readLines(makefile)
+    no_empty_lines <- read[nzchar(read)]
+    no_ws <- trimws(no_empty_lines)
+    no_comments <- no_ws[!grepl("^#", no_ws)]
+    line_break <- gsub("$", " \\\n", no_comments)
+    tokens <- unlist(strsplit(line_break, " "))
+    return(tokens)
+}
