7f6f45fbc44f8fa4cbec6515491356373f7b7547
bleclere <brice.leclere@univ-nantes.fr>
2024-01-13 17:31:46 +0100

    Finish first chapter of R package book

--- R/lexer.R
+++ R/lexer.R
@@ -1,3 +1,13 @@
+#' Lexer for a makefile
+#' 
+#' @param makefile a string containing the relative path of the makefile
+#'
+#' @return A character vector of the tokens of the makefile.
+#' @export
+#' 
+#' @examples
+#' ## NOT RUN
+#' ## tokens <- lexer("path/to/makefile")
 lexer <- function(makefile) {  
     read <- readLines(makefile)
     no_empty_lines <- read[nzchar(read)]
